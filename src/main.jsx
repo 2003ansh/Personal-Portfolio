fe8ce2c42bf97dc39ad2bd995a63ac48027e4f7c
2003ansh <anshumannayak430@gmail.com>
2024-02-06 23:21:17 +0530

    Remove @vercel/speed-insights dependency and update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,7 @@ import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SpeedInsights>
+    
     <App />
-    </SpeedInsights>
   </React.StrictMode>,
 )
