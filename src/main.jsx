d73cff07ecabce92853aff2cbe159c511951a38f
2003ansh <anshumannayak430@gmail.com>
2024-02-06 23:24:50 +0530

    Remove unused import statement

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
